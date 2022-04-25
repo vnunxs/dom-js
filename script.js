e2bc9b1550e258177cd3de39e1faeffc1514b1ea
vnunxs <oerpvividossantos03@gmail.com>
2022-04-25 11:48:09 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function clique(){
+    document.querySelector('#quadrado').innerHTML = "Clicou"
+}
+function pMouse(){
+    document.querySelector('#quadrado').innerHTML = "Passou o mouse"
+}
+function tMouse(){
+    document.querySelector('#quadrado').innerHTML = "Tirou o mouse"
+}
